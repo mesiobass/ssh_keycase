9ab2d0aada6ec98431822c0c27a0c48798dc3bfb
iizuka_local <iizuka@rights-s.jp>
2014-09-10 12:09:10 +0900

    saveコマンド更新

--- scripts/save.bash
+++ scripts/save.bash
@@ -1,12 +1,38 @@
 #!/usr/bin/env bash
 
-. ${SRC_DIR}/functions.bash
+. ${SCRIPT_DIR}/functions.bash
 
-cd ${CONFIG_DIR}
-for filename in $(ls); do
-	printf "%23s %s\n" $(log_success "merge" 1) ${filename}
+declare -a paths
+
+# conf.d/default.confを一番はじめにもってくるようにする
+default_config_path="${CONFIGS_DIR}/${DEFAULT_CONFIG_FILE}"
+[ -e ${default_config_path} ] && paths+=(${default_config_path})
+
+# ファイルパスリストを生成
+fetch_file_paths() {
+	for name in $(ls ${1}); do
+		local full_path="${1}/${name}"
+
+		[ ${full_path} = ${default_config_path} ] && continue
+
+		if [ -d ${full_path} ]; then 
+			fetch_file_paths ${full_path}
+			continue
+		fi
+
+		paths+=(${full_path})
+	done
+}
+
+# 抽出したファイル全てをconfigファイルにマージする
+fetch_file_paths ${CONFIGS_DIR}
+cat ${paths[@]} > ${CONFIG_FILE}
+
+# 実行ログ出力
+for path in ${paths[@]}; do
+	printf "%23s %s\n" $(log_success "merge" 1) ${path}
 done
 
-print_create "config"
+print_create ${CONFIG_FILE}
 
-
+exit $?
