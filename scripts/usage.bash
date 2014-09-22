efa2ec0429e8ff80cafba1635412534bfedde884
iizuka_local <iizuka@rights-s.jp>
2014-09-22 10:59:46 +0900

    ヘルプ更新

--- scripts/usage.bash
+++ scripts/usage.bash
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
 
 cat << _EOT_
-  Usage:
-    keycase [action] [arguments]
+  Usage: keycase [action] [arguments]
+         keycase keyset [action] [arguments]
 
   Action:
-    init [small]    create ssh config directory
-    list            show .ssh/config file
-    show [hostname] show select host infor
-    help            show this message. alias usage
-    usage           show this message. alias help
+    keycase:
+      init               create ssh config directory
+      list               show .ssh/config file
+      show [hostname]    show select host information
+      save               create or update .ssh/config
+      usage              show this message. alias help
+      help               show this message
+      version            show version number
+
+    keycase keyset:
+      create [hostname]  create .ssh/key.d directory and .ssh/conf.d/[hostname].conf file
+
 _EOT_
 
